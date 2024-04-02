9d1449fc614440022b5c7559bfcfa6e17612bc95
Carlos Peña <cepr0190@gmail.com>
2024-04-02 17:34:25 -0300

    config DDBB

--- dist/v1/db/connection.js
+++ dist/v1/db/connection.js
@@ -6,8 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config(); // Load environment variables
+const dataBasePort = () => {
+    return Number(process.env.DB_PORT) ? parseInt(process.env.DB_PORT || '3306') : 3306;
+};
 const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
+    port: dataBasePort(),
     dialect: 'mysql'
 });
 exports.default = sequelize;
--- src/v1/db/connection.ts
+++ src/v1/db/connection.ts
@@ -3,13 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables
 
+const dataBasePort = ()  => {
+   return   Number(process.env.DB_PORT)? parseInt(process.env.DB_PORT||'3306') :3306;
+}
+
 const sequelize = new Sequelize(  
     process.env.DB_NAME|| '',
     process.env.DB_USER|| '',
     process.env.DB_PASSWORD|| '',
     {
-    host:'localhost',
+    host: process.env.DB_HOST || 'localhost',
+    port: dataBasePort(),
     dialect:'mysql'
 })
+
+
 
 export default sequelize;

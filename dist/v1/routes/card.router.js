21c03b807a2e21fb33c8a03453eee4ccfbc30cdb
Carlos Peña <cepr0190@gmail.com>
2024-04-03 22:36:38 -0300

    middleware 4 token

--- dist/v1/routes/card.router.js
+++ dist/v1/routes/card.router.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const card_controller_1 = require("../controllers/card.controller");
+const validate_token_1 = __importDefault(require("../middleware/validate.token"));
 const router = (0, express_1.Router)();
 router.get('/', card_controller_1.getCards);
+router.get('/test', validate_token_1.default, card_controller_1.getCards);
 exports.default = router;
--- src/v1/routes/card.router.ts
+++ src/v1/routes/card.router.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { getCards } from '../controllers/card.controller';
+import validateToken from '../middleware/validate.token';
+
 
 const router = Router();
 
-router.get('/', getCards)
+router.get('/', getCards);
+
+router.get('/test', validateToken, getCards);
+
 
 export default router;

a31527b85a6192b5713dedde2e24ec1bc905111c
Carlos Peña <cepr0190@gmail.com>
2024-07-29 22:51:22 -0300

    --V1.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [
       {
-        name: "balon-3-backend",
-        script: "./dist/index.js",
+        name: "balon3",
+        script: "./dist/index.js", // Asegúrate de que este camino sea correcto después de transpilar TypeScript a JavaScript
         env: {
           NODE_ENV: "development"
         },
